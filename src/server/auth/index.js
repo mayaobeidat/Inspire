cf4010d19260060ec4994fc17a7c9ce99aafbf16
mimmo888 <mayaobeidat@gmail.com>
2024-06-16 19:45:14 -0400

    got hash password to work

--- src/server/auth/index.js
+++ src/server/auth/index.js
@@ -1,51 +1,51 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const { client, getUserById, createUser } = require("../api/db");
-const router = express.Router();
+// const express = require("express");
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcrypt");
+// const { client, getUserByUsername, createUser } = require("./db");
+// const router = express.Router();
 
-const JWT_SECRET = "fashionista"; 
+// const JWT_SECRET = "fashionista"; 
 
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+// function authenticateToken(req, res, next) {
+//   const authHeader = req.headers["authorization"];
+//   const token = authHeader && authHeader.split(" ")[1];
   
-  if (!token) return res.sendStatus(401);
+//   if (!token) return res.sendStatus(401);
   
-  jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-}
-//register
-router.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const hashedPassword = await bcrypt.hash(password, 5);
-    const newUser = await createUser({ username, password: hashedPassword });
-    res.status(201).json({ message: "User successfully created", user: newUser });
-  } catch (err) {
-    res.status(500).json({ message: "User not created" });
-  }
-});
-// login
-router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const user = await getUserById(username);
-    if (!user) return res.status(400).json({ message: "User not found" });
+//   jwt.verify(token, JWT_SECRET, (err, user) => {
+//     if (err) return res.sendStatus(403);
+//     req.user = user;
+//     next();
+//   });
+// }
+// //register
+// router.post("/register", async (req, res) => {
+//   const { username, password } = req.body;
+//   try {
+//     const hashedPassword = await bcrypt.hash(password, 5);
+//     const newUser = await createUser({ username, password: hashedPassword });
+//     res.status(201).json({ message: "User successfully created", user: newUser });
+//   } catch (err) {
+//     res.status(500).json({ message: "User not created" });
+//   }
+// });
+// // login
+// router.post("/login", async (req, res) => {
+//   const { username, password } = req.body;
+//   try {
+//     const user = await getUserByUsername(username);
+//     if (!user) return res.status(400).json({ message: "User not found" });
 
-    const match = await bcrypt.compare(password, user.password);
-    if (match) {
-      const accessToken = jwt.sign({ username: user.username, id: user.id }, JWT_SECRET);
-      res.json({ accessToken });
-    } else {
-      res.status(403).json({ message: "Password is incorrect" });
-    }
-  } catch (err) {
-    res.status(500).json({ message: "Failed log in" });
-  }
-});
+//     const match = await bcrypt.compare(password, user.password);
+//     if (match) {
+//       const accessToken = jwt.sign({ username: user.username, id: user.id }, JWT_SECRET);
+//       res.json({ accessToken });
+//     } else {
+//       res.status(403).json({ message: "Password is incorrect" });
+//     }
+//   } catch (err) {
+//     res.status(500).json({ message: "Failed log in" });
+//   }
+// });
 
-module.exports = { router, authenticateToken };
+// module.exports = { router, authenticateToken };
--- src/server/auth/seed.js
+++ src/server/auth/seed.js
@@ -1,213 +1,177 @@
-const {  
-  client,
-  createUser,
-  updateUser,
-  getAllUsers,
-  getUserById,
-  createPost,
-  updatePost,
-  getAllPosts,
-  getAllTags,
-  getPostsByTagName
-} = require('./index');
+// const { client, getAllUsers } = require("./index");
 
-async function dropTables() {
-  try {
-    console.log("Starting to drop tables...");
+// async function dropTables() {
+//   try {
+//     console.log("Starting to drop tables...");
 
-    // have to make sure to drop in correct order
-    await client.query(`
-      DROP TABLE IF EXISTS post_tags;
-      DROP TABLE IF EXISTS tags;
-      DROP TABLE IF EXISTS bookmarks;
-      DROP TABLE IF EXISTS posts;
-      DROP TABLE IF EXISTS users;
-    `);
+//     // have to make sure to drop in correct order
+//     await client.query(`
+//         DROP TABLE IF EXISTS order_products;
+//         DROP TABLE IF EXISTS orders;
+//         DROP TABLE IF EXISTS products;
+//         DROP TABLE IF EXISTS users;
+//       `);
 
-    console.log("Finished dropping tables!");
-  } catch (error) {
-    console.error("Error dropping tables!");
-    throw error;
-  }
-}
+//     console.log("Finished dropping tables!");
+//   } catch (error) {
+//     console.error("Error dropping tables!");
+//     throw error;
+//   }
+// }
 
-async function createTables() {
-  try {
-    console.log("Starting to build tables...");
+// async function createTables() {
+//   try {
+//     console.log("Starting to build tables...");
 
-    await client.query(`
-      CREATE TABLE users (
-        id SERIAL PRIMARY KEY,
-        username varchar(255) UNIQUE NOT NULL,
-        password varchar(255) NOT NULL,
-        name varchar(255) NOT NULL,
-        location varchar(255) NOT NULL,
-        active boolean DEFAULT true
-      );
+//     await client.query(`
+//         CREATE TABLE "users" (
+//         "id" SERIAL NOT NULL,
+//         "name" text NOT NULL,
+//         "email" text NOT NULL,
+//         "address" text NOT NULL,
+//         "username" text UNIQUE NOT NULL,
+//         "password" text NOT NULL,
+//         PRIMARY KEY ("id")
+//         );
 
-      CREATE TABLE posts (
-        id SERIAL PRIMARY KEY,
-        "authorId" INTEGER REFERENCES users(id),
-        title varchar(255) NOT NULL,
-        content TEXT NOT NULL,
-        active BOOLEAN DEFAULT true
-      );
+//         CREATE TABLE "products" (
+//         "id" SERIAL NOT NULL,
+//         "name" text NOT NULL,
+//         "image_url" varchar NOT NULL,
+//         "description" text NOT NULL,
+//         "price" numeric NOT NULL,
+//         "categories" text NOT NULL,
+//         // "availability" bool NOT NULL DEFAULT true,
+//         PRIMARY KEY ("id")
+//         );
 
-      CREATE TABLE tags (
-        id SERIAL PRIMARY KEY,
-        name varchar(255) UNIQUE NOT NULL
-      );
+//         CREATE TABLE "orders" (
+//         "id" SERIAL NOT NULL,
+//         "customer_id" int4 NOT NULL,
+//         "is_cart" bool NOT NULL DEFAULT true,
+//         "ordered_at" timestamptz NOT NULL DEFAULT now(),
+//         "date_time_purchased" timestamptz NOT NULL DEFAULT now(),
+//         PRIMARY KEY ("id")
+//         );
 
-      CREATE TABLE post_tags (
-        "postId" INTEGER REFERENCES posts(id),
-        "tagId" INTEGER REFERENCES tags(id),
-        UNIQUE ("postId", "tagId")
-      );
+//         CREATE TABLE "order_products" (
+//           "id" SERIAL NOT NULL,
+//           "order_id" int4 NOT NULL,
+//           "product_id" int4 NOT NULL,
+//           "quantity" numeric NOT NULL,
+//           PRIMARY KEY ("id")
+//           );
+  
+//       `);
 
-      CREATE TABLE bookmarks (
-        id SERIAL PRIMARY KEY,
-        "postId" INTEGER REFERENCES posts(id),
-        "userId" INTEGER REFERENCES users(id),
-        UNIQUE ("postId", "userId")
-        );
-    `);
+//     console.log("Finished building tables!");
+//   } catch (error) {
+//     console.error("Error building tables!");
+//     throw error;
+//   }
+// }
 
-    console.log("Finished building tables!");
-  } catch (error) {
-    console.error("Error building tables!");
-    throw error;
-  }
-}
+// async function createInitialUsers() {
+//   try {
+//     console.log("Starting to create users...");
 
-async function createInitialUsers() {
-  try {
-    console.log("Starting to create users...");
+//     await client.query(`
+//         CREATE UNIQUE INDEX users_username_idx ON users USING btree (username);
 
-    await createUser({ 
-      username: 'albert', 
-      password: 'bertie99',
-      name: 'Al Bert',
-      location: 'Sidney, Australia' 
-    });
-    await createUser({ 
-      username: 'sandra', 
-      password: '2sandy4me',
-      name: 'Just Sandra',
-      location: 'Ain\'t tellin\''
-    });
-    await createUser({ 
-      username: 'glamgal',
-      password: 'soglam',
-      name: 'Joshua',
-      location: 'Upper East Side'
-    });
-    
-    console.log("Finished creating users!");
-  } catch (error) {
-    console.error("Error creating users!");
-    throw error;
-  }
-}
+//         INSERT INTO "users" ("username", "password", "email", "phone_number", "created_at", "updated_at") VALUES
+//         ('sam23', 'pass123', 'sam233@gmail.com', '718-928-2383', '2024-06-13 14:28:44.156062-04', '2024-06-13 14:28:44.156062-04'),
+//         ('peter2434', 'pass333', 'pete5@gmail.com', '888-923-3283', '2024-06-13 14:28:44.156062-04', '2024-06-13 14:28:44.156062-04'),
+//         ('matt343', 'pass444', 'mparker56@gmail.com', '029-238-2382', '2024-06-13 14:28:44.156062-04', '2024-06-13 14:28:44.156062-04');
+//         `);
+//     console.log("Finished creating users!");
+//   } catch (error) {
+//     console.error("Error creating users!");
+//     throw error;
+//   }
+// }
+// async function createInitialProducts() {
+//   await client.query(`
+//             INSERT INTO "products" ("price", "description", "name", "categories", "image_url", "availability") VALUES
+//             (5.99, 'fruit', 'blueberries', 'produce', 'https://i5.walmartimages.com/asr/206db476-ca7d-4919-a4a5-c25dd2ae5feb_1.f7927999cdacc320a6c5a83462cadbd5.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF', 't'),
+//             (1.99, 'fruit', 'bannas', 'produce', 'https://www.walmart.com/ip/Marketside-Fresh-Organic-Bananas-Bunch/51259338?athbdg=L1200', 't'),
+//             (5.44, 'fruit', 'peaches', 'produce', 'https://www.walmart.com/ip/Fresh-Organic-Peaches-2-lb-Bag/157005099', 't'),
+//             (4.92, 'fruit', 'lemon', 'produce', 'https://www.walmart.com/ip/Fresh-Organic-Lemons-2-lb-Bag/51259193', 't'),
+//             (3.47, 'fruit', 'strawberries', 'produce', 'https://www.walmart.com/ip/Fresh-USDA-Organic-Strawberries-1-lb-Container/45618183', 't'),
+//             (3.66, 'fruit', 'Pink Lady Apples', 'produce', 'https://www.walmart.com/ip/Fresh-Organic-Pink-Lady-Apples-2-lb-Pouch/51259207', 't'),
+//             (8.12, 'fruit', 'fuji apples', 'produce', 'https://www.walmart.com/ip/Fresh-Organic-Fuji-Apples-3-lb-Bag/165846014', 't'),
+//             (1.66, 'vegetable', 'Baby Carrots', 'produce', 'https://www.walmart.com/ip/Organic-Marketside-Fresh-Baby-Peeled-Carrots-1-lb-Bag/51259199', 't'),
+//             (1.99, 'vegetable', 'Onions', 'produce', 'https://www.walmart.com/ip/Fresh-Yellow-Onions-3-lb-Bag/10447842', 't'),
+//             (0.88, 'vegetable', 'cilantro', 'produce', 'https://www.walmart.com/ip/Fresh-Cilantro-Bunch/160597260', 't'),
+//             (2.99, 'vegetable', 'broccoli', 'produce', ' https://www.walmart.com/ip/Savor-Brands-Grade-A-Broccoli-Cuts-20-Pound-1-each/374312923?from=/search', 't'),
+//             (3.99, 'vegetable', 'green beans', 'produce', ' https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.eatingwell.com%2Fthmb%2FzOSGCalh6otXC12446RNP8_TeBM%3D%2F1500x0%2Ffilters%3Ano_upscale()%3Amax_bytes(150000)%3Astrip_icc()%2Fquick-easy-green-beans-step-2-9d1ca0c6d9f94332be6f371e257106da.jpg&tbnid=fjLw8FBJfjShvM&vet=12ahUKEwilg82RjtmGAxVQD2IAHYM4CwkQMygBegUIARCyAQ..i&imgrefurl=https%3A%2F%2Fwww.eatingwell.com%2Frecipe%2F362784%2Fquick-easy-green-beans%2F&docid=XQby3dbpEQBpMM&w=1500&h=1500&q=green%20beans&ved=2ahUKEwilg82RjtmGAxVQD2IAHYM4CwkQMygBegUIARCyAQ', 't'),
+//             (2.99, 'vegetable', 'cauliflower', 'produce', 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F2%2F2f%2FChou-fleur_02.jpg&tbnid=jTLnX-4gjJsJ1M&vet=12ahUKEwi0xuqkjtmGAxVFOlkFHUHDBBAQMygDegQIARBt..i&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCauliflower&docid=it6iEba4DxmQbM&w=4320&h=3456&q=cauliflower&ved=2ahUKEwi0xuqkjtmGAxVFOlkFHUHDBBAQMygDegQIARBt', 't'),
+//             (5.99, 'fruit', 'kiwi', 'produce', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/02/Kiwi-fruits-582a07b.jpg?quality=90&resize=556,505', 't'),
+//             (7.99, 'fruit', 'cherries', 'produce', 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fblog.lexmed.com%2Fimages%2Flibrariesprovider80%2Fblog-post-featured-images%2Fshutterstock_1933299218.jpg%3Fsfvrsn%3D4a8d900b_0&tbnid=XRwrOMUTKqeYkM&vet=12ahUKEwi3xrD8jtmGAxXiBmIAHYfaCCUQMygEegUIARCyAQ..i&imgrefurl=https%3A%2F%2Fblog.lexmed.com%2Fhome%2Fblog%2F2022%2F11%2F01%2Fsuperfood-of-the-month-cherries&docid=KZWe2fE4iYnO_M&w=1900&h=1267&q=cherries&ved=2ahUKEwi3xrD8jtmGAxXiBmIAHYfaCCUQMygEegUIARCyAQ', 't'),
+//             (10.99, 'fruit', 'watermelon', 'produce', 'https://cdn.britannica.com/99/143599-050-C3289491/Watermelon.jpg', 't'),
+//             (5.99, 'fruit', 'grape', 'produce', 'https://www.fourwindsgrowers.com/cdn/shop/products/blackmonukka_1024x1024.jpg?v=1538780984', 't')
+// `);
+// }
+// async function createInitialCartItems() {
+//   try {
+//     console.log("Starting to create cart items...");
+//     await client.query(`
+//         INSERT INTO "cart" ("product_id", "customer_id") VALUES
+//         (27, 3),
+//         (28, 1),
+//         (14, 3)
+//         `);
+//     console.log("Finished creating cart items!");
+//   } catch (error) {
+//     console.log("Error creating cart items!");
+//     throw error;
+//   }
+// }
+// async function createInitialOrderedItems() {
+//   try {
+//     console.log("Starting to create ordered items...");
+//     await client.query(`
+//         INSERT INTO "orders" ("product_id", "customer_id") VALUES
+//         (14, 3)
+//         `);
+//     console.log("Finished creating ordered items!");
+//   } catch (error) {
+//     console.log("Error creating ordered items!");
+//     throw error;
+//   }
+// }
 
-async function createInitialPosts() {
-  try {
-    const [albert, sandra, glamgal] = await getAllUsers();
+// async function rebuildDB() {
+//   try {
+//     client.connect();
 
-    console.log("Starting to create posts...");
-    await createPost({
-      authorId: albert.id,
-      title: "First Post",
-      content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
-      tags: ["#happy", "#youcandoanything"]
-    });
+//     await dropTables();
+//     await createTables();
+//     await createInitialUsers();
+//     await createInitialCartItems();
+//     await createInitialProducts();
+//     await createInitialOrderedItems();
+//   } catch (error) {
+//     console.log("Error during rebuildDB");
+//     throw error;
+//   }
+// }
 
-    await createPost({
-      authorId: sandra.id,
-      title: "How does this work?",
-      content: "Seriously, does this even do anything?",
-      tags: ["#happy", "#worst-day-ever"]
-    });
+// async function testDB() {
+//   try {
+//     console.log("Starting to test database...");
 
-    await createPost({
-      authorId: glamgal.id,
-      title: "Living the Glam Life",
-      content: "Do you even? I swear that half of you are posing.",
-      tags: ["#happy", "#youcandoanything", "#canmandoeverything"]
-    });
-    console.log("Finished creating posts!");
-  } catch (error) {
-    console.log("Error creating posts!");
-    throw error;
-  }
-}
+//     console.log("Calling getAllUsers");
+//     const users = await getAllUsers();
+//     console.log("Result:", users);
 
-async function rebuildDB() {
-  try {
-    client.connect();
+//     console.log("Finished database tests!");
+//   } catch (error) {
+//     console.log("Error during testDB");
+//     throw error;
+//   }
+// }
 
-    await dropTables();
-    await createTables();
-    await createInitialUsers();
-    await createInitialPosts();
-  } catch (error) {
-    console.log("Error during rebuildDB")
-    throw error;
-  }
-}
-
-async function testDB() {
-  try {
-    console.log("Starting to test database...");
-
-    console.log("Calling getAllUsers");
-    const users = await getAllUsers();
-    console.log("Result:", users);
-
-    console.log("Calling updateUser on users[0]");
-    const updateUserResult = await updateUser(users[0].id, {
-      name: "Newname Sogood",
-      location: "Lesterville, KY"
-    });
-    console.log("Result:", updateUserResult);
-
-    console.log("Calling getAllPosts");
-    const posts = await getAllPosts();
-    console.log("Result:", posts);
-
-    console.log("Calling updatePost on posts[0]");
-    const updatePostResult = await updatePost(posts[0].id, {
-      title: "New Title",
-      content: "Updated Content"
-    });
-    console.log("Result:", updatePostResult);
-
-    console.log("Calling updatePost on posts[1], only updating tags");
-    const updatePostTagsResult = await updatePost(posts[1].id, {
-      tags: ["#youcandoanything", "#redfish", "#bluefish"]
-    });
-    console.log("Result:", updatePostTagsResult);
-
-    console.log("Calling getUserById with 1");
-    const albert = await getUserById(1);
-    console.log("Result:", albert);
-
-    console.log("Calling getAllTags");
-    const allTags = await getAllTags();
-    console.log("Result:", allTags);
-
-    console.log("Calling getPostsByTagName with #happy");
-    const postsWithHappy = await getPostsByTagName("#happy");
-    console.log("Result:", postsWithHappy);
-
-    console.log("Finished database tests!");
-  } catch (error) {
-    console.log("Error during testDB");
-    throw error;
-  }
-}
-
-
-rebuildDB()
-  .then(testDB)
-  .catch(console.error)
-  .finally(() => client.end());
+// rebuildDB()
+//   .then(testDB)
+//   .catch(console.error)
+//   .finally(() => client.end());
