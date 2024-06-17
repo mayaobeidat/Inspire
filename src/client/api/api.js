5af91c0b12fe963a3c009e0ce5a25c1f3509b50d
mimmo888 <mayaobeidat@gmail.com>
2024-06-17 14:51:44 -0400

    resubmitting api file due to merge issue

---
+++ src/client/api/api.js
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi ({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery ({
+        baseUrl: 'http://localhost:8080/',
+        prepareHeaders: (headers, { getState }) => {
+            const token = getState().auth.token;
+            if (token) {
+                headers.set('authorization', `Bearer ${token}`)
+            }
+            headers.set('Content-Type', 'application/json');
+            return headers;
+        },
+    }),
+    endpoints: ()=> ({})
+});
