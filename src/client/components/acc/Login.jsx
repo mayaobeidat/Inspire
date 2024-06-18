8253db535d26aedf89ab156e924b46ee08244d16
mimmo888 <mayaobeidat@gmail.com>
2024-06-18 10:01:27 -0400

    updated andy's merge

--- src/client/components/acc/Login.jsx
+++ src/client/components/acc/Login.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useLoginMutation } from "..api/sliceAuth";
+import { useLoginMutation } from "../../api/sliceAuth";
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
