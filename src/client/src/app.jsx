66f75bf0ef62fc95b030342017b69db6bb86f77f
mimmo888 <mayaobeidat@gmail.com>
2024-06-18 11:54:50 -0400

    installs

--- src/client/src/app.jsx
+++ src/client/src/app.jsx
@@ -4,20 +4,55 @@
 //     );
 // };
 // export default App;
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import AllProducts from './allProducts';
-import SingleProduct from './singleProduct';
 
-const App = () => {
+
+// import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import AllProducts from './components/products/allProducts';
+// import SingleProduct from './components/products/singleProduct';
+
+// function App () {
+//   return (
+//     <div>
+//       <Routes>
+//         <Route path="/api/products/:id" element={SingleProduct} />
+//         <Route path="/api/products" element={AllProducts} />
+//       </Routes>
+//     </div>
+//   );
+// };
+
+// export default App;
+// App.jsx
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setToken } from "./api/sliceAuth";
+import { Routes, Route } from "react-router-dom";
+import AllProducts from "./components/products/allProducts";
+import SingleProduct from "./components/products/singleProduct";
+import "./index.css";
+
+function App() {
+  const [userToken, setUserToken] = useState({});
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    console.log("token", token)
+    if (token) {
+      dispatch(setToken(token));
+      setUserToken(token);
+    }
+  }, [dispatch]);
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/product/:id" component={singleProduct} />
-        <Route path="/" component={allProducts} />
-      </Switch>
-    </Router>
+    <div className="main">
+      <Routes>
+        <Route path="/" element={<AllProducts />} />
+        <Route path="/:id" element={<SingleProduct />} />
+      </Routes>
+    </div>
   );
-};
+}
 
 export default App;
