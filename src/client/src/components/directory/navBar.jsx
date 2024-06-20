9205c7ae12e1c2600a2d4b70ee31188c26577c74
lukeharris3 <166353470+lukeharris3@users.noreply.github.com>
2024-06-20 10:11:01 -0600

    Update navBar.jsx

--- src/client/src/components/directory/navBar.jsx
+++ src/client/src/components/directory/navBar.jsx
@@ -1,11 +1,9 @@
-/* TODO - add your code to create a functional React component that renders a navigation bar for the different views in your single page application. You may consider conditionally rendering some options - for example 'Login' should be available if someone has not logged in yet. */
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink, useNavigate } from "react-router-dom";
 import { clearToken } from "../../api/sliceAuth";
 
 export default function NavBar() {
-  const token = useSelector((state) => state.auth);
-//   .token after auth? fix
+  const token = useSelector((state) => state.auth.token); 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -25,6 +23,11 @@ export default function NavBar() {
         <li>
           <NavLink className="nlB" to="/">
             Products
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="nlC" to="/cart">
+            Cart
           </NavLink>
         </li>
         {token ? (
